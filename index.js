b3e423eebfeb557d455e18b7438130f56d6979c5
xoak <54974852+xoak@users.noreply.github.com>
2020-03-31 00:15:51 -0700

    Add getTitle function

---
+++ index.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+const apiUrl = 'https://www.googleapis.com/youtube/v3';
+
+async function getTitle(videoId, apiKey) {
+  try {
+    const response = await axios.get(
+      apiUrl + '/videos?part=snippet&id=' + videoId + '&key=' + apiKey
+    );
+    console.log(response.data.items[0].snippet.title);
+    return response.data.items[0].snippet.title;
+  } catch (error) {
+    console.error(error);
+  }
+}
